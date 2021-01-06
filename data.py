3441eed529403a9ffdaa1122ba007b7ca56fb609
Pius <plee@ucsd.edu>
2021-01-05 19:52:48 -0800

    Initial commit

---
+++ data.py
@@ -0,0 +1,14 @@
+import requests
+
+parameters={
+    "amount":10,
+    "type":"boolean"
+
+}
+
+response=requests.get("https://opentdb.com/api.php?amount=10&category=15&difficulty=hard&type=boolean",params=parameters)
+response.raise_for_status()
+data=response.json()
+
+
+question_data = data["results"]
